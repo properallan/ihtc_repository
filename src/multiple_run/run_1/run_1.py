27f63ca185b06246194152e59fd7746d258dc9b4
properallan <properallan@gmail.com>
2023-04-19 15:17:26 -0300

    last uptade with environment.yml

--- src/multiple_run/run_1/run_1.py
+++ src/multiple_run/run_1/run_1.py
@@ -15,14 +15,14 @@ def gen_dataset(model, dataset_root,  doe_file, index_range, other_params):
 if __name__ == "__main__":
     import sys
     import os
-    sys.path.append('/home/ppiper/ihtc_repository/src')
-    sys.path.append('/home/ppiper/ihtc_repository/src')
+    sys.path.append('/home/ppiper/Dropbox/local/ihtc_repository/src')
+    sys.path.append('/home/ppiper/Dropbox/local/ihtc_repository/src')
     from models_parametric_shape import *
     pass
-    doe_file = '/home/ppiper/ihtc_repository/data/doe_test/doe_lhs.txt'
-    hf_params = {'Nx': '210', 'Ny': '330', 'tol': '1e-8', 'cores': 'None', 'inflationRate': '1.0015', 'metal': 'AISI406', 'itmaxSU2': '3_000', 'GMSH_SOLVER': '/home/ppiper/ihtc_repository/src/gmsh', 'SU2_SOLVER': '/home/ppiper/ihtc_repository/src/SU2_CFD', 'baselineCP': '/home/ppiper/ihtc_repository/src/baselineCP.txt', 'T0in': '600.0', 'p0in': '8.0e5', 'itprintSU2': '1'}
-    lf_params = {'Nx': '400', 'EULER_Q1D_SOLVER': '/home/ppiper/ihtc_repository/src/eulerQ1D', 'baselineCP': '/home/ppiper/ihtc_repository/src/baselineCP.txt', 'T0in': '600.0', 'p0in': '8.0e5'}
-    index_range = [1]
-    dataset_root = '/home/ppiper/ihtc_repository/data/doe_test'
+    doe_file = '/home/ppiper/Dropbox/local/ihtc_repository/data/doe_30/doe_lhs.txt'
+    hf_params = {'Nx': '210', 'Ny': '330', 'tol': '1e-16', 'cores': 'None', 'inflationRate': '1.0015', 'metal': 'AISI406', 'itmaxSU2': '3_000', 'GMSH_SOLVER': '/home/ppiper/Dropbox/local/ihtc_repository/src/gmsh', 'SU2_SOLVER': '/home/ppiper/Dropbox/local/ihtc_repository/src/SU2_CFD', 'baselineCP': '/home/ppiper/Dropbox/local/ihtc_repository/src/baselineCP.txt', 'T0in': '600.0', 'p0in': '8.0e5', 'itprintSU2': '1'}
+    lf_params = {'Nx': '400', 'EULER_Q1D_SOLVER': '/home/ppiper/Dropbox/local/ihtc_repository/src/eulerQ1D', 'baselineCP': '/home/ppiper/Dropbox/local/ihtc_repository/src/baselineCP.txt', 'T0in': '600.0', 'p0in': '8.0e5'}
+    index_range = [1, 2, 3, 4, 5, 6, 7, 8]
+    dataset_root = '/home/ppiper/Dropbox/local/ihtc_repository/data/doe_30'
     gen_dataset(lf_model, dataset_root, doe_file,index_range,  lf_params )
     gen_dataset(hf_model, dataset_root, doe_file,index_range,  hf_params )
